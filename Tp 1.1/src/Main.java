62c8a043431db51c2aff4d8d25004d6b764f571b
Manshiur <manshiurylunita@gmail.com>
2025-04-14 02:23:18 -0300

    Punto 1 TP 1

---
+++ Tp 1.1/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int Nota;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nota: ");
+        Nota = sc.nextInt();
+        if (Nota >= 0 && Nota < 4){
+            System.out.println("Desaprobaste la materia");
+        } else if (Nota==10) {
+            System.out.println("Usted obtuvo un Sobresaliente, Felicitaciones!");
+
+        }
+        else if (Nota > 10 || Nota < 0) {
+            System.out.println("El número que ingreso no es correcto, saliendo del programa...");
+        } else {
+            System.out.println("Usted aprobó la materia, sigue así.");
+        }
+    }
+}
